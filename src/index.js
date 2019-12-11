b1a4fc8b2cfbe7ee92838e60b419dfe3be8874d2
Nikas <nikas@Nikas-MBP.lan>
2019-12-11 17:38:24 +0200

    fetch is working now

--- src/index.js
+++ src/index.js
@@ -1,44 +1,33 @@
-const getWeatherData = () => {
-  let options = {
-    mode: 'cors'
-  };
+const { createDomElement, addToDom } = require('./utilities')
 
-  fetch('http://82.140.185.11/v1/places/vilnius/forecasts/long-term', options)
-  .then((resp) => resp.json())
-  .then(
-      function(data) {
-          console.log(data)
+function getWeatherData() {
+
+  fetch('https://cors-anywhere.herokuapp.com/http://api.meteo.lt/v1/places/vilnius/forecasts/long-term')
+    .then((resp) => resp.json())
+    .then(
+      function (data) {
+        const app = document.getElementById('weatherApp');
+
+        app.innerHTML = null;
+
+        const locationCard = createDomElement('div', {
+          className: 'locationCard'
+        });
+
+        const cityName = createDomElement('h4', { textContent: data.place.name });
+        const temperature = createDomElement('h2', { innerHTML: data.forecastTimestamps[0].airTemperature + ' &#8451;' });
+
+        addToDom(app, locationCard);
+        addToDom(locationCard, temperature);
+        addToDom(locationCard, cityName);
+        console.log(data)
       }
-  )
-  .catch (
+    )
+    .catch(
       function (e) {
-          console.log(e);
+        console.log(e);
       }
-  );
+    );
 };
 
 getWeatherData();
-
-const weatherDataFetched = require('./getData');
-const {createDomElement, addToDom} = require('./utilities')
-
-function render() {
-  const app = document.getElementById('weatherApp');
-
-  app.innerHTML = null;
-
-  const locationCard = createDomElement('div', {
-    className: 'locationCard'
-  });
-
-  const cityName = createDomElement('h4', {textContent: weatherDataFetched.weatherData.place.name});
-  const temperature = createDomElement('h2', {innerHTML: weatherDataFetched.weatherData.forecastTimestamps[0].airTemperature + ' &#8451;'});
-
-  addToDom(app, locationCard);
-  addToDom(locationCard, temperature);
-  addToDom(locationCard, cityName);
-
-
-};
-
-render();
